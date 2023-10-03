890ff901278669b28a64df4d8b55a6128c695d53
ainugiri12 <ainugiri@gmail.com>
2023-10-03 13:51:50 +0530

    changes_giri_oct

--- Sample.java
+++ Sample.java
@@ -1,9 +1,9 @@
 class Sample{
 
 public static void main(String[] arg){
-System.out.println("Giri code pulled to local repo and compiled");
-System.out.println("Giri code pulled to local repo and compiled");
-System.out.println("Giri code pulled to local repo and compiled");
+System.out.println("Giri Prasad  code pulled to local repo and compiled");
+System.out.println("Giri Prasad code pulled to local repo and compiled");
+System.out.println("Giri Prasad code pulled to local repo and compiled");
 System.out.println("Giri code pulled to local repo and compiled");
 System.out.println("Giri code pulled to local repo and compiled");
 }
