6a4e94954fa5b50bb4dc4fbde8b44cab3c9ef5c0
ainugiri12 <ainugiri@gmail.com>
2023-10-03 13:56:03 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,5 @@ class Sample{
 public static void main(String[] arg){
 System.out.println("Giri Prasad  code pulled to local repo and compiled");
 System.out.println("Giri Prasad code pulled to local repo and compiled");
-System.out.println("Giri Prasad code pulled to local repo and compiled");
 }
 }
